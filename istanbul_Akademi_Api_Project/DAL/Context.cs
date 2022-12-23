f15aa08ae73cebc6e3fbdd2c9b6cc06bed4e4133
Yunus Kalyoncu <95497265+YunusKalyoncu@users.noreply.github.com>
2022-12-23 22:51:00 +0300

    Initial commit

---
+++ istanbul_Akademi_Api_Project/DAL/Context.cs
@@ -0,0 +1,18 @@
+﻿using istanbul_Akademi_Api_Project.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace istanbul_Akademi_Api_Project.DAL
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=YUNUS\\SQLEXPRESS;initial catalog=ApiDb;integrated security=true;");
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ istanbul_Akademi_Api_Project/DAL/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace istanbul_Akademi_Api_Project.DAL.Entities
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+    }
+}
